815b3e024ddcd3ac9d31c36fbc56ec1a53b819cf
Bruna Marques <brunamarques@cloudinfo.pt>
2022-05-17 23:19:25 +0100

    WIP layout

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -34,7 +34,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     _getGifs().then((map) {
-      print(map);
+      // print(map);
     });
   }
 
@@ -48,6 +48,119 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
+      body: _body(),
+      backgroundColor: const Color.fromARGB(255, 32, 38, 46),
+    );
+  }
+
+  _body() {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(6)),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 20, 23, 27),
+                  Color.fromARGB(255, 16, 17, 19)
+                ],
+                stops: [0.0, 0.8],
+              ),
+            ),
+            height: 160,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Search",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 26,
+                    color: Color.fromARGB(182, 234, 234, 234),
+                  ),
+                ),
+                _searchBar(),
+              ],
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                switch (snapshot.connectionState) {
+                  case (ConnectionState.waiting):
+                  case (ConnectionState.none):
+                    return Container(
+                      width: 200,
+                      height: 200,
+                      alignment: Alignment.center,
+                      child: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError) {
+                      return Container();
+                    } else {
+                      return _createGifTable(context, snapshot);
+                    }
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _searchBar() {
+    return const TextField(
+      style: TextStyle(color: Color.fromARGB(255, 234, 234, 234)),
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.search_rounded,
+          color: Color.fromARGB(182, 234, 234, 234),
+        ),
+        hintText: "cute dog",
+        hintStyle: TextStyle(
+          color: Color.fromARGB(182, 234, 234, 234),
+          fontSize: 14,
+          fontStyle: FontStyle.italic,
+        ),
+        labelStyle: TextStyle(color: Color.fromARGB(255, 234, 234, 234)),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+        filled: true,
+        fillColor: Color.fromARGB(95, 41, 44, 48),
+        // focusedBorder: InputBorder(),
+      ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: const EdgeInsets.only(top: 30),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, int index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
     );
   }
 }
